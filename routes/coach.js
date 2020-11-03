d46539be6cc32eb687e765b27fdbefd04068ddff
burnca02 <62625876+burnca02@users.noreply.github.com>
2020-11-02 22:55:21 -0500

    Update coach.js
    Updated post function for /upload so we don't need upload.js

--- routes/coach.js
+++ routes/coach.js
@@ -4,7 +4,7 @@ const { ensureAuthenticated } = require('../config/auth');
 //const csv = require('fast-csv');
 var fileUpload = require('express-fileupload');
 var template = require('../template');
-var upload = require('../upload');
+//var upload = require('../upload');
 const csv = require('fast-csv');
 var mongoose = require('mongoose');
 const Roster = require('../models/Roster');
@@ -21,59 +21,34 @@ mongoose.connect(db, { useNewUrlParser: true ,useUnifiedTopology: true})
 .then(() => console.log('Mongo DB Connected...'))
 .catch(err => console.log(err));
 
-// const multer = require('multer');
-// const upload = multer({
-//     dest: 'uploads/' // this saves your file into a directory called "uploads"
-//   });
-  
-// var storage = multer.diskStorage({  
-//     destination:(req,file,cb)=>{  
-//         cb(null,'./uploads');  
-//     },  
-//     filename:(req,file,cb)=>{  
-//         cb(null,file.originalname);  
-//     }  
-// });
-// var uploads = multer({storage:storage}); 
-//var csv         = require('csvtojson');
-
 router.get('/template', template.get);
 
-router.post('/upload', upload.post);
-//upload function
-// router.post('/upload', (req,res) => 
-// {
-//     if (!req.files)
-//        return res.status(400).send('No files were uploaded.');
-    
-//     var rosterFile = req.files.file;
+router.post('/upload', (req,res) => {
+    if (!req.files)
+            return res.status(400).send('No files were uploaded.');
+        
+        var rosterFile = req.files.file;
 
-//     var players = [];
-        
-//     csv.parseString(rosterFile.data.toString(), {
-//         headers: true,
-//         ignoreEmpty: true
-//     })
-//     .on("data", function(data){
-//         const newPlayer = new Player({
-//           GradYear,
-//           FirstName,
-//           LastName,
-//           Position
-//         })
-//         //data['_id'] = new mongoose.Player();
-        
-//         players.push(data);
-//     })
-//     .on("end", function(){
-//         Roster.create(players, function(err, documents) {
-//             if (err) throw err;
-//         });
-//                 // Sorting by the position Alhpabetically.
-//         res.redirect('/coachHome');
-//     });
-    
-// })
+        var players = [];
+            
+        csv.parseString(rosterFile.data.toString(), {
+            headers: true,
+            ignoreEmpty: true
+        })
+        .on("data", function(data){
+            data['_id'] = new mongoose.Types.ObjectId();
+            
+            players.push(data);
+        })
+        .on("end", function(){
+            Roster.create(players, function(err, documents) {
+                if (err) throw err;
+            });
+
+        });
+        console.log("Uploaded to database");
+        res.redirect('/coach/upload');
+});
 
 // router.post('/upload',uploads.single('csv'),(req,res)=>{  
 //   //convert csvfile to jsonArray     

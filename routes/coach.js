c7e347a4a130fab9ea30ea13b1dd03e7df381f2e
burnca02 <62625876+burnca02@users.noreply.github.com>
2020-11-05 10:21:04 -0500

    Update coach.js
    Added get methods for playerFeedback, questionnaire, and upload. Got rid of commented out code that wasn't working.

--- routes/coach.js
+++ routes/coach.js
@@ -21,23 +21,22 @@ mongoose.connect(db, { useNewUrlParser: true ,useUnifiedTopology: true})
 .then(() => console.log('Mongo DB Connected...'))
 .catch(err => console.log(err));
 
-// const multer = require('multer');
-// const upload = multer({
-//     dest: 'uploads/' // this saves your file into a directory called "uploads"
-//   });
-  
-// var storage = multer.diskStorage({  
-//     destination:(req,file,cb)=>{  
-//         cb(null,'./uploads');  
-//     },  
-//     filename:(req,file,cb)=>{  
-//         cb(null,file.originalname);  
-//     }  
-// });
-// var uploads = multer({storage:storage}); 
-//var csv         = require('csvtojson');
-
 router.get('/template', template.get);
+
+router.get('/upload', ensureAuthenticated, (req, res) => 
+  res.render('coachHome', {
+    name: req.user.name //pass the name that was entered into the database to dashboard
+}));
+
+router.get('/playerFeedback', ensureAuthenticated, (req, res) => 
+  res.render('playerFeedback', {
+    name: req.user.name //pass the name that was entered into the database to dashboard
+}));
+
+router.get('/questionnaire', ensureAuthenticated, (req, res) => 
+  res.render('questionnaire', {
+    name: req.user.name //pass the name that was entered into the database to dashboard
+}));
 
 router.post('/upload', upload.post);
 //upload function
@@ -74,75 +73,5 @@ router.post('/upload', upload.post);
 //     });
     
 // })
-
-// router.post('/upload',uploads.single('csv'),(req,res)=>{  
-//   //convert csvfile to jsonArray     
-//  csv()  
-//  .fromFile(req.file.path)  
-//  .then((jsonObj)=>{  
-//      console.log(jsonObj);  
-//      //the jsonObj will contain all the data in JSONFormat.
-//      //but we want columns Test1,Test2,Test3,Test4,Final data as number .
-//      //becuase we set the dataType of these fields as Number in our mongoose.Schema(). 
-//      //here we put a for loop and change these column value in number from string using parseFloat(). 
-//      //here we use parseFloat() beause because these fields contain the float values.
-//      for(var x=0;x<jsonObj;x++){  
-//           temp = parseFloat(jsonObj[x].Test1)  
-//           jsonObj[x].Test1 = temp;  
-//           temp = parseFloat(jsonObj[x].Test2)  
-//           jsonObj[x].Test2 = temp;  
-//           temp = parseFloat(jsonObj[x].Test3)  
-//           jsonObj[x].Test3 = temp;  
-//           temp = parseFloat(jsonObj[x].Test4)  
-//           jsonObj[x].Test4 = temp;  
-//           temp = parseFloat(jsonObj[x].Final)  
-//           jsonObj[x].Final = temp;  
-//       } 
-//       //insertmany is used to save bulk data in database.
-//      //saving the data in collection(table)
-//       csvModel.insertMany(jsonObj,(err,data)=>{  
-//              if(err){  
-//                  console.log(err);  
-//              }else{  
-//                  res.redirect('/coachHome');  
-//              }  
-//       });  
-//     });  
-//  });  
-
-//  //insertmany is used to save bulk data in database.
-//     //saving the data in collection(table)
-//     Player.insertMany(jsonObj,(err,data)=>{  
-//       if(err){  
-//           console.log(err);  
-//       }else{  
-//           res.redirect('/');  
-//       }  
-// });
-
-// router.post = function (req, res) {
-//   if (!req.files)
-//       return res.status(400).send('No files were uploaded.');
-   
-//   var rosterFile = req.files.file;
-
-//   var players = [];
-       
-//   csv.parseString(rosterFile.data.toString(), {
-//        headers: true,
-//        ignoreEmpty: true
-//    })
-//    .on("data", function(data){
-//        data['_id'] = new mongoose.Types.ObjectId();
-        
-//        players.push(data);
-//    })
-//    .on("end", function(){
-//        Roster.create(players, function(err, documents) {
-//           if (err) throw err;
-//        });
-//        res.redirect('/');
-//    });
-// };
 
 module.exports = router;

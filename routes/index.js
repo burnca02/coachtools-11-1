9d81d139c512ef5d6c7928325dc6bc561af18dd3
Ricardo Hernandez <40433585+hernri01@users.noreply.github.com>
2020-11-11 22:36:11 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const { ensureAuthenticated } = require('../config/auth');
 const Questionnaire = require('../models/Questionnaire');
 const CompleteQuest = require('../models/CompleteQuest');
+const MongoClient = require('mongodb').MongoClient
+const uri = "mongodb+srv://hernri01:Capstone2020@cluster0.3ln2m.mongodb.net/test?authSource=admin&replicaSet=atlas-9q0n4l-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true&useUnifiedTopology=true";
 
 //Welcome Page
 router.get('/', (req, res) => res.render('welcome'));
@@ -10,7 +12,7 @@ router.get('/', (req, res) => res.render('welcome'));
 router.get('/test', (req, res) => res.render('test'));
 
 router.get('/viewQuestionnaire', (req, res) => 
-Questionnaire.find({}).sort({type: 'meeting'}).limit(1) //gets most recent doc, need to change so that all come through
+Questionnaire.find({"type": "meeting"}).limit(1) //gets most recent doc, need to change so that all come through
 .then(questionnaires => {
   console.log(questionnaires);
   if(questionnaires.length == 0){
@@ -50,12 +52,27 @@ router.post('/viewQuestionnaire', (req, res) => {
     .catch(err => console.log(err));
   });
 
-//Coach Home Page
-router.get('/coachHome', ensureAuthenticated, (req, res) => 
-    res.render('coachHome', {
-        name: req.user.name //pass the name that was entered into the database to dashboard
-    })
-);
+
+  MongoClient.connect(uri, { useUnifiedTopology: true })
+  .then(client => {
+      const db = client.db('test');
+      const rosterCollection = db.collection('Roster');
+  
+          //Coach Home Page`
+      router.get('/coachHome', ensureAuthenticated, (req, res) => 
+  
+      db.collection('Roster').find({ "Pos": { "$exists": true }, "School": req.session.school}).sort({'Pos': 1}).toArray()
+      .then(results => {
+          res.render('coachHome', {players: results,
+                                  name: req.user.name,
+                                  school: req.session.school   
+                                  }
+                   )
+      })
+      .catch(error => console.error(error))
+      );
+  })
+  .catch(console.error)
 
 //Player Home Page
 router.get('/playerHome', ensureAuthenticated, (req, res) =>

3fac20cb3d0996f1ab1a5f58db00c8431574000b
Ricardo Hernandez <40433585+hernri01@users.noreply.github.com>
2020-11-11 22:24:52 -0500

    Update player.js

--- routes/player.js
+++ routes/player.js
@@ -6,7 +6,7 @@ const Stat = require('../models/Stat');
 
 //Connect DB again??
 const mongoose = require('mongoose');
-const db = 'mongodb+srv://yoda:maytheforce@cluster0.fvmkx.mongodb.net/test?retryWrites=true&w=majority';
+const db = 'mongodb+srv://hernri01:Capstone2020@cluster0.3ln2m.mongodb.net/test?authSource=admin&replicaSet=atlas-9q0n4l-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true&useUnifiedTopology=true&useNewUrlParser=true';
 mongoose.connect(db, { useNewUrlParser: true ,useUnifiedTopology: true})
 .then(() => console.log('Mongo DB Connected...'))
 .catch(err => console.log(err));
@@ -43,19 +43,29 @@ router.get('/practiceGrade', (req, res) => res.render('practiceGrade'));
 
 
 //player trends page
-router.get('/playerTrends', ensureAuthenticated, (req, res, next) => 
-  Stat.findOne({ email: req.user.email }).sort({$natural:-1}).limit(1)
-  .then(stat => {
-    console.log(stat.bench);
-    res.render('playerTrends', {
+router.get('/playerTrends', ensureAuthenticated, (req, res) => 
+
+  Stat.findOne({ email: req.session.email }).sort({createdAt:-1}).limit(1) // Query to find the most recent stat for the user.
+  .then(stat => 
+  {
+    Stat.find({ email: req.session.email }).sort({createdAt:1}) //This query will be used to populate the graph.
+    .then(stats =>
+    {
+      console.log(stat.bench);
+      res.render('playerTrends', 
+        {
           email: stat.email,
           bench: stat.bench,
           squat: stat.squat,
           dead: stat.dead,
           mile: stat.mile,
           height: stat.height,
-          weight: stat.weight
+          weight: stat.weight,
+          graph: stats
         });
+    })
+        console.log(req.session);
+
   }
 ));
 

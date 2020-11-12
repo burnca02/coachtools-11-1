9ebcb40b0376772b25d1e61e19b16d4adaf1fcf1
kmurdough <36051815+kmurdough@users.noreply.github.com>
2020-11-12 10:20:59 -0500

    added static files folder
    included all statics in layout.ejs so everything should work in other files

---
+++ public/js/site.js
@@ -0,0 +1,23 @@
+function hideSubs() {
+    var pos = ["sQB", "sRB", "sWR"]; //,"LT","LG","C", "RG","RT","TE","CB","DE","DT","OLB","MLB","SS","FS"];
+    for (i = 0; i < pos.length; i++) {
+      var hideDiv = document.getElementById(pos[i]);
+      hideDiv.style.display = "none";
+    }
+  }
+
+  function showSubs(sub) {
+    hideSubs();
+    var showDiv = document.getElementById(sub);
+    showDiv.style.display = "block";
+  }
+  
+  function renameFunc(n) {
+    var orig = document.getElementById(n).innerHTML;
+    var person = prompt("Please enter your name:");
+    if (person != "" && person != null) {
+      document.getElementById(n).innerHTML = person;
+    } else {
+      document.getElementById(n).innerHTML = orig;
+    }
+  }

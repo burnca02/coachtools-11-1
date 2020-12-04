b39a954259cb655d1c13612712b1f0a4c03ee3ee
burnca02 <burnca02@gburg.edu>
2020-12-03 21:06:01 -0500

    config documentation

--- config/passport.js
+++ config/passport.js
@@ -1,3 +1,4 @@
+//Stes up passport.js authentication strategy. See passport.js documentation for serialize and deserialize description.
 const LocalStrategy = require('passport-local').Strategy;
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');

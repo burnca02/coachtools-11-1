4b7397c143b66dbe37665aafab625168ad0a8c54
Kayl <kgmurdough@gmail.com>
2021-01-18 15:27:57 -0500

    sidenav and pwa fixes
    updated some of the sidenav buttons and added a filler image for the calendar
    changed the icon for apple iphone app for progressive web app so it should look better

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,7 @@
 self.addEventListener("install", e => {
     e.waitUntil(
         caches.open("static").then(cache => {
-            return cache.addAll(["images/coachtoolsLogoBlack614.png", "/users/login", "/", "/aboutUs", "/contactUs", "css/style.css"]);
+            return cache.addAll(["images/coachtoolsLogoBlack614.png", "images/coachtoolsLogoBlack.png", "/users/login", "/", "/aboutUs", "/contactUs", "css/style.css"]);
         })
     );
 });

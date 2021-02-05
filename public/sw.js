3f376e98c87609f3786d32f0f0c0feecf3365f16
Kayl <kgmurdough@gmail.com>
2021-02-04 20:05:39 -0500

    depthChart update
    stable update

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,9 @@
+// service worker creation
+// needed for the app to be installed/created
 self.addEventListener("install", e => {
     e.waitUntil(
         caches.open("static").then(cache => {
+            // this line caches certain files that can be accessed without wifi - DONT PUT FILES IN HERE THAT CHANGE FREQUENTLY
             return cache.addAll(["images/coachtoolsLogoBlack614.png", "images/coachtoolsLogoBlack.png"]);  //, "/users/login", "/", "/aboutUs", "/contactUs", "css/style.css"
         })
     );

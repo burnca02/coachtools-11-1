b6a5b2b78a58c97e2b1a51483a4c8486556e8c60
Kayl <kgmurdough@gmail.com>
2021-02-05 10:36:45 -0500

    delete static cache

--- public/sw.js
+++ public/sw.js
@@ -3,8 +3,9 @@
 self.addEventListener("install", e => {
     e.waitUntil(
         caches.open("static").then(cache => {
+            self.caches.delete('static');
             // this line caches certain files that can be accessed without wifi - DONT PUT FILES IN HERE THAT CHANGE FREQUENTLY
-            return cache.addAll(["images/coachtoolsLogoBlack614.png", "images/coachtoolsLogoBlack.png"]);  //, "/users/login", "/", "/aboutUs", "/contactUs", "css/style.css"
+            return cache.addAll(["images/coachtoolsLogoBlack.png"]);  //"images/coachtoolsLogoBlack614.png", , "/users/login", "/", "/aboutUs", "/contactUs", "css/style.css"
         })
     );
 });
